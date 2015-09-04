d63f10f66405984002236d0c0db2fc3c641ad607
David de Tena <daviddetenacalvo@gmail.com>
2015-09-04 19:59:26 +0200

    Model and View are now synchronized and the screen displays the Wine info.

--- app/src/main/java/com/daviddetena/baccus/controller/WineActivity.java
+++ app/src/main/java/com/daviddetena/baccus/controller/WineActivity.java
@@ -3,7 +3,10 @@ package com.daviddetena.baccus.controller;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.daviddetena.baccus.R;
 import com.daviddetena.baccus.model.Wine;
@@ -11,8 +14,20 @@ import com.daviddetena.baccus.model.Wine;
 public class WineActivity extends Activity {
 
     private static final String TAG = WineActivity.class.getSimpleName();
-    private ImageView mWineImage = null;
+
+
+    // Modelo
     private Wine mWine = null;
+
+    // Vistas
+    private ImageView mWineImage = null;
+    private TextView mWineNameText = null;
+    private TextView mWineTypeText = null;
+    private TextView mWineOriginText = null;
+    private RatingBar mWineRatingBar = null;
+    private TextView mWineCompanyText = null;
+    private TextView mWineNotesText = null;
+    private ViewGroup mWineGrapesContainer = null;
 
 
     @Override
@@ -20,19 +35,42 @@ public class WineActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine);
 
-        mWine = new Wine("Vegaval", "Tinto", R.drawable.vegaval, "", "", "", "Valdepeñas",5);
+        // Creamos el modelo
+        mWine = new Wine("Bembibre",
+                "Tinto",
+                R.drawable.vegaval,
+                "Dominio de Tares",
+                "http://www.dominiodetares.com/portfolio/bembibre/",
+                "Este vino muestra toda la complejidad y la elegancia de la variedad Mencía. En fase visual luce un color rojo picota muy cubierto con tonalidades violáceas en el menisco. En nariz aparecen recuerdos frutales muy intensos de frutas rojas (frambuesa, cereza) y una potente ciruela negra, así como tonos florales de la gama de las rosas y violetas, vegetales muy elegantes y complementarios, hojarasca verde, tabaco y maderas aromáticas (sándalo) que le brindan un toque ciertamente perfumado.",
+                "El Bierzo",
+                5);
 
-        // Buscamos la referencia a la imagen en el .xml y se la asignamos a la propiedad
+        // Accedemos a las vistas desde el controlador, utilizando sus referencias en el .xml, para
+        // asociarlas a nuestras variables
         mWineImage = (ImageView) findViewById(R.id.wine_image);
+        mWineNameText = (TextView) findViewById(R.id.wine_name);
+        mWineTypeText = (TextView) findViewById(R.id.wine_type);
+        mWineOriginText = (TextView) findViewById(R.id.wine_origin);
+        mWineRatingBar = (RatingBar) findViewById(R.id.wine_rating);
+        mWineCompanyText = (TextView) findViewById(R.id.wine_company);
+        mWineNotesText = (TextView) findViewById(R.id.wine_notes);
+        mWineGrapesContainer = (ViewGroup) findViewById(R.id.grapes_container);
 
+        // Sincronizamos modelo y vista
+        syncModelWithView();
     }
 
     /**
-     * Método que se ejecuta cuando se pulsa en el botón de CAMBIAR
-     * Queremos que cambie la imagen sólo al pulsar el botón
-     * @param view
+     * Método helper que utilizamos para mostrar en las vistas la información de nuestro modelo de
+     * vino.
      */
-    public void changeImage(View view){
+    private void syncModelWithView(){
         mWineImage.setImageResource(mWine.getPhoto());
+        mWineNameText.setText(mWine.getName());
+        mWineTypeText.setText(mWine.getType());
+        mWineOriginText.setText(mWine.getOrigin());
+        mWineRatingBar.setRating(mWine.getRating());
+        mWineCompanyText.setText(mWine.getCompanyName());
+        mWineNotesText.setText(mWine.getNotes());
     }
 }
